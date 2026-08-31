3f823812baa4da5aa27a9e3549161123eeb302e9
Pratiksha Raut <pratikshabraut46@gmail.com>
2026-08-31 22:56:05 +0530

    Implement ArrayListSum

---
+++ ArrayListSum.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ArrayListSum {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            sum += numbers.get(i);
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
